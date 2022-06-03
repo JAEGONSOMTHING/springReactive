ca8d39a43103d60d10d5070a3ecd0e48999a13d9
IM JAE GON <89837407+jaegonSex@users.noreply.github.com>
2022-06-03 19:22:37 +0900

    Initial commit

---
+++ src/main/java/com/jaegon/reacivestudy/ReacivestudyApplication.java
@@ -0,0 +1,13 @@
+package com.jaegon.reacivestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReacivestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReacivestudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaegon/reacivestudy/ReacivestudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaegon.reacivestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReacivestudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
